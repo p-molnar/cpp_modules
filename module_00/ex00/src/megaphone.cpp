80fca0e557fae6bef617dde195374333b8a77e32
Peter Molnar <pmolnar@f1r4s12.codam.nl>
2023-09-26 12:18:57 +0200

    Simplify program

--- module_00/ex00/src/megaphone.cpp
+++ module_00/ex00/src/megaphone.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <cstring>
 
-int	main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	std::string curr_arg{};
+
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < argc; i++)
 	{
-		int len = strlen(argv[i]);
-		for (int j = 0; j < len; j++)
-			argv[i][j] = toupper(argv[i][j]);
-		std::cout << argv[i];
+		curr_arg = argv[i];
+		for (int j = 0, len = curr_arg.length(); j < len; j++)
+			std::cout << static_cast<char>(std::toupper(curr_arg[j]));
 	}
 	std::cout << '\n';
 	return EXIT_SUCCESS;
