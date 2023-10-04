90d7ca186c37272e56434bf85429934bb7957f92
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-10-04 13:23:06 +0200

    Add solution for ex03

---
+++ module_01/ex03/src/HumanB.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+#include "Weapon.hpp"
+#include "HumanB.hpp"
+
+HumanB::HumanB(std::string name)
+	: name(name), weapon(nullptr) {}
+
+HumanB::HumanB(std::string name, Weapon *weapon)
+	: name(name), weapon(weapon) {}
+
+void HumanB::setWeapon(Weapon &weapon)
+{
+	this->weapon = &weapon;
+}
+
+void HumanB::attack(void)
+{
+	if (weapon)
+		std::cout << name << " attacks with their " << weapon->getType();
+	else
+		std::cout << name << " doesn't have a weapon to attack with";
+	std::cout << std::endl;
+}
---
+++ module_01/ex03/src/Weapon.cpp
@@ -0,0 +1,15 @@
+#include "Weapon.hpp"
+#include <string>
+
+Weapon::Weapon(std::string type)
+	: type(type){};
+
+std::string Weapon::getType(void)
+{
+	return type;
+}
+
+void Weapon::setType(std::string type)
+{
+	this->type = type;
+}
