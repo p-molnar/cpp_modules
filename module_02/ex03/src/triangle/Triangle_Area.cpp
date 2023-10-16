b75639f3aaf7cad76d3a10edd36e073dfc096c5a
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-10-16 23:57:05 +0200

    Remove redundant code

--- module_02/ex03/src/triangle/Triangle_Area.cpp
+++ module_02/ex03/src/triangle/Triangle_Area.cpp
@@ -1,50 +1,6 @@
 #include "Point.hpp"
 #include "Triangle.hpp"
 #include "Fixed.hpp"
-#include <math.h>
-
-typedef struct PtAgl
-{
-	Point p;
-	Fixed angle;
-} PtAgl;
-
-Point Triangle::getCenterPoint(const Triangle &t)
-{
-	Fixed center_x = (t.v1.getX() + t.v2.getX() + t.v3.getX()) / 3;
-	Fixed center_y = (t.v1.getY() + t.v2.getY() + t.v3.getY()) / 3;
-
-	return Point(center_x, center_y);
-}
-
-Point *Triangle::getSortedVerticesCW()
-{
-	Point center = getCenterPoint(*this);
-	Fixed agl_1 = atan2((v1.getY() - center.getY()).toFloat(), (v1.getX() - center.getX()).toFloat());
-	Fixed agl_2 = atan2((v2.getY() - center.getY()).toFloat(), (v2.getX() - center.getX()).toFloat());
-	Fixed agl_3 = atan2((v3.getY() - center.getY()).toFloat(), (v3.getX() - center.getX()).toFloat());
-
-	PtAgl pt_agl[3] = {{v1, agl_1},
-					   {v2, agl_2},
-					   {v3, agl_3}};
-
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = i + 1; j < 3; j++)
-		{
-			if (pt_agl[i].angle < pt_agl[j].angle)
-			{
-				PtAgl tmp = pt_agl[i];
-				pt_agl[i] = pt_agl[j];
-				pt_agl[j] = tmp;
-			}
-		}
-	}
-	Point *v_sorted = new Point[3];
-	for (int i = 0; i < 3; i++)
-		v_sorted[i] = pt_agl[i].p;
-	return v_sorted;
-}
 
 bool Triangle::isPointInside(const Point &p)
 {
