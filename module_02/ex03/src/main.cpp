aff87d492b499f73197fed64c7cbad3868bf3f64
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-10-17 12:36:02 +0200

    Add explanatory comments

--- module_02/ex03/src/main.cpp
+++ module_02/ex03/src/main.cpp
@@ -13,11 +13,14 @@ int main(void)
 {
 	// atexit(f);
 
+	// must come in counter clock wise order
+	// a point placed on the side of the triangle is deemed to be inside
+	// the triangle
 	Point p1(0, 0);
 	Point p2(5, 0);
 	Point p3(0, 5);
 
-	Point p(2, 2);
+	Point p(0, 5);
 
 	Triangle triangle(p1, p2, p3);
 
