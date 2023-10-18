5436128edbefdcd5082301cfebf360be74eea68e
Peter Molnar <pmolnar@f1r4s12.codam.nl>
2023-10-18 12:02:25 +0200

    Add getter member functions

---
+++ module_03/ex00/src/ClapTrap_getters.cpp
@@ -0,0 +1,5 @@
+#include "ClapTrap.hpp"
+
+int ClapTrap::getEnergyPt(void) const { return energy_pt; }
+int ClapTrap::getHealthPt(void) const { return health_pt; }
+int ClapTrap::getDamagePt(void) const { return damage_pt; }
