9f1173daa99fc2629872b5885dccd3bccb3c16f2
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-10-20 12:42:19 +0200

    Remove redundant code, set values to specified ones

--- module_03/ex01/src/ScavTrap/ScavTrap.cpp
+++ module_03/ex01/src/ScavTrap/ScavTrap.cpp
@@ -11,38 +11,13 @@ ScavTrap::ScavTrap(void)
 ScavTrap::ScavTrap(std::string name)
 	: ClapTrap(name)
 {
-	this->setDamagePt(0);
 	std::cout << "String constructor of ScavTrap " << name << " called\n";
+	this->setHealthPt(100);
+	this->setEnergyPt(50);
+	this->setDamagePt(20);
 }
-
-// ScavTrap::ScavTrap(const ScavTrap &obj)
-// {
-// 	std::cout << "Copy constructor of Scavtrap" << obj.getName() << " called\n";
-// 	this->setName(obj.getName());
-// 	this->setHealthPt(obj.getHealthPt());
-// 	this->setEnergyPt(obj.getEnergyPt());
-// 	this->setDamagePt(obj.getDamagePt());
-// }
-// const ScavTrap &ScavTrap::operator=(const ScavTrap &obj)
-// {
-// 	std::cout << "Copy assignment constructor of ScavTrap" << obj.getName() << " called\n";
-// 	this->setName(obj.getName());
-// 	this->setHealthPt(obj.getHealthPt());
-// 	this->setEnergyPt(obj.getEnergyPt());
-// 	this->setDamagePt(obj.getDamagePt());
-// 	return *this;
-// }
 
 ScavTrap::~ScavTrap(void)
 {
 	std::cout << "Destructor of ScavTrap " << this->getName() << " called\n";
 }
-
-// std::ostream &operator<<(std::ostream &os, const ScavTrap &obj)
-// {
-// 	std::cout << "Name: " << obj.getName() << '\n';
-// 	std::cout << "health_pt: " << obj.getHealthPt() << '\n';
-// 	std::cout << "energy_pt: " << obj.getEnergyPt() << '\n';
-// 	std::cout << "damage_pt: " << obj.getDamagePt() << '\n';
-// 	return os;
-// }
