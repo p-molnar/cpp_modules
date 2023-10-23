757fe37eaeb5810f5b98e6de64bfc65d5f74642f
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-10-23 12:02:12 +0200

    Add more test cases

--- module_03/ex02/src/main.cpp
+++ module_03/ex02/src/main.cpp
@@ -7,8 +7,25 @@ int main(void)
 	FragTrap a("A");
 
 	std::cout << a << '\n';
-	a.takeDamage(100);
+	a.takeDamage(80);
+	std::cout << a << '\n';
 	a.attack("B");
+	std::cout << a << '\n';
+	a.highFivesGuys();
+	std::cout << a << '\n';
+	a.attack("B");
+	std::cout << a << '\n';
+	a.beRepaired(30);
+	std::cout << a << '\n';
+	a.attack("B");
+	a.beRepaired(67);
+	std::cout << a << '\n';
+	a.beRepaired(67);
+	std::cout << a << '\n';
+	a.beRepaired(1);
+	std::cout << a << '\n';
+	a.takeDamage(200);
+	std::cout << a << '\n';
 	a.highFivesGuys();
 	std::cout << a << '\n';
 
