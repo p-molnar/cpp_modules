9badeedbbdaff4fb7beac109d1e105554a55a919
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-10-23 12:10:17 +0200

    Add more test cases

--- module_03/ex03/src/main.cpp
+++ module_03/ex03/src/main.cpp
@@ -6,18 +6,33 @@ int main(void)
 {
 	DiamondTrap a("A");
 
+	std::cout << a << '\n';
+	a.takeDamage(80);
+	std::cout << a << '\n';
 	a.attack("B");
-	a.printMembers();
-	a.takeDamage(20);
-	a.printMembers();
-	a.beRepaired(15);
-	a.printMembers();
-
-	a.whoAmI();
-
-	a.guardGate();
-
+	std::cout << a << '\n';
 	a.highFivesGuys();
+	std::cout << a << '\n';
+	a.guardGate();
+	std::cout << a << '\n';
+	a.attack("B");
+	std::cout << a << '\n';
+	a.beRepaired(30);
+	std::cout << a << '\n';
+	a.attack("B");
+	std::cout << a << '\n';
+	a.beRepaired(67);
+	std::cout << a << '\n';
+	a.beRepaired(67);
+	std::cout << a << '\n';
+	a.beRepaired(1);
+	std::cout << a << '\n';
+	a.takeDamage(200);
+	std::cout << a << '\n';
+	a.highFivesGuys();
+	std::cout << a << '\n';
+	a.guardGate();
+	std::cout << a << '\n';
 
 	return EXIT_SUCCESS;
 }
