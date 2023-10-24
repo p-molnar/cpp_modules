9ea578f05af2a9106bad47b959b2568caebbd1c7
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-10-24 09:53:52 +0200

    Add test cases

--- module_04/ex00/src/main.cpp
+++ module_04/ex00/src/main.cpp
@@ -1,21 +1,41 @@
 #include "Animal.hpp"
+#include "WrongAnimal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongCat.hpp"
 #include <cstdlib>
 #include <iostream>
 
 int main()
 {
 	const Animal *meta = new Animal();
-	const Animal *j = new Dog();
-	const Animal *i = new Cat();
+	const Animal *dog = new Dog();
+	const Animal *cat = new Cat();
 
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
+	std::cout << dog->getType() << " " << std::endl;
+	std::cout << cat->getType() << " " << std::endl;
 
-	i->makeSound(); // will output the cat sound!
-	j->makeSound();
+	std::cout << "cat makes sound: ";
+	cat->makeSound();
+	std::cout << "dog makes sound: ";
+	dog->makeSound();
+	std::cout << "animal makes sound: ";
 	meta->makeSound();
 
+	delete meta;
+	delete dog;
+	delete cat;
+
+	const WrongAnimal *wrong_meta = new WrongAnimal();
+	const WrongAnimal *wrong_cat = new WrongCat();
+
+	std::cout << wrong_cat->getType() << " " << std::endl;
+
+	std::cout << "wrong cat makes sound: ";
+	wrong_cat->makeSound();
+	std::cout << "wrong animal makes sound: ";
+	wrong_meta->makeSound();
+	delete wrong_meta;
+	delete wrong_cat;
 	return EXIT_SUCCESS;
 }
