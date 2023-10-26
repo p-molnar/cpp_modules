50e9342cbae516eacbc2901ebe1a60500f1088da
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-10-26 14:29:01 +0200

    Add more tests

--- module_04/ex01/src/main.cpp
+++ module_04/ex01/src/main.cpp
@@ -8,25 +8,51 @@ void leaks(void)
 	system("leaks brain");
 }
 
+int Animal::c_id = 0;
 int main(void)
 {
 	atexit(leaks);
-	Animal *cat = new Cat;
-	std::cout << '\n';
-	Animal *dog = new Dog;
+	// tests for virtual destructor
+
+	const int size = 5;
+	Animal **animals = new Animal *[size];
 	std::cout << '\n';
 
-	Dog *dog_2 = new Dog;
-	std::cout << '\n';
-	Dog *dog_3;
-	std::cout << '\n';
-	dog_3 = dog_2;
-	(void)dog_3;
-	std::cout << cat->getType() << '\n';
-	std::cout << dog->getType() << '\n';
+	for (int i = 0; i < size; i++)
+	{
+		if (i < size / 2)
+		{
+			std::cout << "dog #" << i << '\n';
+			animals[i] = new Dog;
+			std::cout << '\n';
+		}
+		else
+		{
+			std::cout << "cat #" << i << '\n';
+			animals[i] = new Cat;
+			std::cout << '\n';
+		}
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		delete animals[i];
+	}
+	delete[] animals;
+
+	// test for deep copies
+	Dog dog_a;
+	{
+		Dog dog_b;
+		std::cout << '\n';
+		std::cout << "dog #" << dog_a.id << " brain address: " << dog_a.getBrainAddress() << '\n';
+		std::cout << "dog #" << dog_a.id << " brain idea: " << dog_a.getBrain() << '\n';
+		std::cout << "dog #" << dog_b.id << " brain address: " << dog_b.getBrainAddress() << '\n';
+		std::cout << "dog #" << dog_b.id << " brain idea: " << dog_b.getBrain() << '\n';
+		dog_a = dog_b;
+	}
 
 	std::cout << '\n';
-	delete cat;
-	std::cout << '\n';
-	delete dog;
+	std::cout << "dog #" << dog_a.id << " brain address: " << dog_a.getBrainAddress() << '\n';
+	std::cout << "dog #" << dog_a.id << " brain's mem address: " << dog_a.getBrain() << '\n';
 }
