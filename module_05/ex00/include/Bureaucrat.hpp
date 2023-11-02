47a227a4586e51d81da010babe30232dda429d6d
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-11-02 21:57:03 +0100

    Simplify code with nested classes
    Remove individual exception classes and their dedicated files, and
    nest the exception classes into base class.

--- module_05/ex00/include/Bureaucrat.hpp
+++ module_05/ex00/include/Bureaucrat.hpp
@@ -2,15 +2,28 @@
 #define BUREAUCRAT__HPP
 
 #include <string>
-#include "GradeTooLowException.hpp"
-#include "GradeTooHighException.hpp"
 #include <exception>
 
 #define HIGHEST_GRADE 1
 #define LOWEST_GRADE 150
 
-class Bureaucrat : public GradeTooHighException, GradeTooLowException
+class Bureaucrat
 {
+
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		GradeTooHighException(){};
+		virtual const char *what() const throw() { return "Grade too high\n"; }
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		GradeTooLowException(void){};
+		virtual const char *what(void) const throw() { return "Grade too low\n"; }
+	};
+
 private:
 	const std::string name;
 	int grade;
@@ -20,7 +33,7 @@ public:
 	Bureaucrat(std::string name, int grade);
 	Bureaucrat(const Bureaucrat &obj);
 	Bureaucrat &operator=(const Bureaucrat &obj);
-	virtual ~Bureaucrat(void) throw();
+	~Bureaucrat(void) throw();
 
 public:
 	const std::string &getName(void) const;
