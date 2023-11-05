7b0e42e5fb483a4ea08008e0a9e0b6598693b041
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-11-06 00:15:11 +0100

    Add test cases

--- module_05/ex01/src/main.cpp
+++ module_05/ex01/src/main.cpp
@@ -1,53 +1,98 @@
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
 #include <cstdlib>
 #include <iostream>
 
 int main(void)
 {
-	// try
-	// {
-	// 	Bureaucrat B_1("Sophie", 0);
-	// }
-	// catch (std::exception &e)
-	// {
-	// 	std::cout << "exception: " << e.what();
-	// }
+	// Tests from ex00
+	try
+	{
+		Bureaucrat B_1("Sophie", 0);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "exception: " << e.what();
+	}
 
-	// try
-	// {
-	// 	Bureaucrat B_2("Sophie", 151);
-	// }
-	// catch (std::exception &e)
-	// {
-	// 	std::cout << "exception: " << e.what();
-	// }
+	try
+	{
+		Bureaucrat B_2("Sophie", 151);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "exception: " << e.what();
+	}
 
-	// Bureaucrat B_3("Sophie", 1);
-	// try
-	// {
-	// 	B_3.increment();
-	// }
-	// catch (std::exception &e)
-	// {
-	// 	std::cout << "exception: " << e.what();
-	// }
+	Bureaucrat B_3("Sophie", 1);
+	try
+	{
+		B_3.increment();
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "exception: " << e.what();
+	}
 
-	// Bureaucrat B_4("Sophie", 150);
-	// try
-	// {
-	// 	B_4.decrement();
-	// }
-	// catch (std::exception &e)
-	// {
-	// 	std::cout << "exception: " << e.what();
-	// }
+	Bureaucrat B_4("Sophie", 150);
+	try
+	{
+		B_4.decrement();
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "exception: " << e.what();
+	}
 
-	// Bureaucrat B_5("Sophie", 40);
-	// std::cout << B_5 << '\n';
-	// B_5.increment();
-	// std::cout << B_5 << '\n';
-	// B_5.decrement();
-	// std::cout << B_5 << '\n';
+	Bureaucrat B_5("Sophie", 40);
+	std::cout << B_5 << '\n';
+	B_5.increment();
+	std::cout << B_5 << '\n';
+	B_5.decrement();
+	std::cout << B_5 << '\n';
 
+	// tests for exercise ex01
+
+	Form F_1("Form_1", 2, 4);
+
+	try
+	{
+		Form F_2("Form_2", 0, 4);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what();
+	}
+
+	try
+	{
+		Form F_3("Form_3", 2, 151);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what();
+	}
+
+	try
+	{
+		Form F_4("Contract", 5, 8);
+		Bureaucrat B_6("Sarah", 4);
+		B_6.signForm(F_4);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what();
+	}
+
+	try
+	{
+		Form F_5("Consultancy Agreement", 2, 8);
+		Bureaucrat B_7("Sarah", 4);
+		B_7.signForm(F_5);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what();
+	}
 	return EXIT_SUCCESS;
 }
