2026d84a91216aa441ac4c55fa80a9b37e18912e
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-11-26 23:10:11 +0100

    Add tests

--- module_07/ex00/src/main.cpp
+++ module_07/ex00/src/main.cpp
@@ -2,9 +2,27 @@
 #include "min.hpp"
 #include "swap.hpp"
 #include <iostream>
+#include <cstdlib>
 
 int main(void)
 {
+	{
+		int a = 2;
+		int b = 3;
+		::swap(a, b);
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+		std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+		std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	}
+
+	std::cout << std::endl;
+
 	{
 		int a = 1;
 		int b = 3;
@@ -41,4 +59,5 @@ int main(void)
 		swap(a, b);
 		std::cout << "a = " << a << "; b = " << b << '\n';
 	}
+	return EXIT_SUCCESS;
 }
