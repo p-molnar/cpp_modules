fa76a380e6e46ef28c9f71a5c6cc905984c2e0c6
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-11-28 16:35:15 +0100

    Add test cases

--- module_08/ex01/src/main.cpp
+++ module_08/ex01/src/main.cpp
@@ -5,14 +5,55 @@ int main(void)
 {
 	{
 		Span sp = Span(5);
+		try
+		{
+			sp.addNumber(6);
+			sp.addNumber(3);
+			sp.addNumber(17);
+			sp.addNumber(9);
+			sp.addNumber(11);
 
-		sp.addNumber(6);
-		sp.addNumber(3);
-		sp.addNumber(17);
-		sp.addNumber(9);
-		sp.addNumber(11);
+			std::cout << sp.shortestSpan() << std::endl;
+			std::cout << sp.longestSpan() << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << '\n';
+		}
+	}
 
-		std::cout << sp.shortestSpan() << std::endl;
-		std::cout << sp.longestSpan() << std::endl;
+	std::cout << std::endl;
+
+	{
+		Span sp = Span(5);
+		try
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				sp.addNumber(i);
+			}
+			std::cout << sp.shortestSpan() << std::endl;
+			std::cout << sp.longestSpan() << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << '\n';
+		}
+	}
+
+	std::cout << std::endl;
+
+	{
+		Span sp = Span(1);
+		sp.addNumber(2);
+		try
+		{
+			std::cout << sp.shortestSpan() << std::endl;
+			std::cout << sp.longestSpan() << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << '\n';
+		}
 	}
 }
