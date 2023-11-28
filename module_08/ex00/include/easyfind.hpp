eeaef9e54f58cb3c93105a593528c62b78d6cc20
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-11-28 12:30:12 +0100

    Remove line causing compl. err.

--- module_08/ex00/include/easyfind.hpp
+++ module_08/ex00/include/easyfind.hpp
@@ -10,14 +10,11 @@ int easyfind(const C &container, int lookup_val)
 	typename C::const_iterator ite = container.cend();
 	unsigned int idx = 0;
 
-	if (std::is_same<T, int>::value)
-		throw
-
-			for (it = container.cbegin(); it != ite; ++it, idx++)
-		{
-			if (*it == lookup_val)
-				return idx;
-		}
+	for (it = container.cbegin(); it != ite; ++it, idx++)
+	{
+		if (*it == lookup_val)
+			return idx;
+	}
 	throw(std::runtime_error("value not found"));
 }
 
