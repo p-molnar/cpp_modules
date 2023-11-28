65680ad2e09e853a0a94fa6e177830111c3b3160
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-11-28 16:37:52 +0100

    Update exception msg

--- module_08/ex01/src/Span/Span_longestSpan.cpp
+++ module_08/ex01/src/Span/Span_longestSpan.cpp
@@ -5,7 +5,7 @@
 unsigned int Span::longestSpan(void)
 {
 	if (elements.size() < 2)
-		throw std::runtime_error("No span can be found");
+		throw std::runtime_error("no span can be found");
 
 	int min_el = *std::min_element(elements.begin(), elements.end());
 	int max_el = *std::max_element(elements.begin(), elements.end());
