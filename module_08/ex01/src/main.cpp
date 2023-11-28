abaa1378c8ec1df181effe06a379ad69e5652c20
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-11-28 16:37:49 +0100

    Add test cases

--- module_08/ex01/src/main.cpp
+++ module_08/ex01/src/main.cpp
@@ -45,9 +45,26 @@ int main(void)
 
 	{
 		Span sp = Span(1);
-		sp.addNumber(2);
 		try
 		{
+			sp.addNumber(2);
+			sp.addNumber(3);
+			std::cout << sp.shortestSpan() << std::endl;
+			std::cout << sp.longestSpan() << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << '\n';
+		}
+	}
+
+	std::cout << std::endl;
+
+	{
+		Span sp = Span(1);
+		try
+		{
+			sp.addNumber(2);
 			std::cout << sp.shortestSpan() << std::endl;
 			std::cout << sp.longestSpan() << std::endl;
 		}
